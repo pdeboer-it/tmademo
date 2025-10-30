cc104ffa51122e92153f14889475a4bcb5fee7d3
pdeboer-it <info@pdeboer-it.com>
2025-10-30 09:59:06 +0100

    Update baseUrl function to use HTTPS for production API endpoint

--- src/app/src/utils/env.ts
+++ src/app/src/utils/env.ts
@@ -1,7 +1,5 @@
 export function baseUrl() {
-  console.log(import.meta.env.MODE)
-
   return import.meta.env.MODE === 'development'
     ? 'http://localhost:5280'
-    : 'tmademoapi-e9brgbcacubfgqa3.westeurope-01.azurewebsites.net'
+    : 'https://tmademoapi-e9brgbcacubfgqa3.westeurope-01.azurewebsites.net'
 }
